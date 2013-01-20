7429d32efd4e24dfeedb4c569c1a369517ce1b96
Guillaume Bokiau <guillaume.bokiau@gmail.com>
2013-01-20 22:40:19 +0100

    renaming adminmenu option as menu

--- Controller/Component/AdminComponent.php
+++ Controller/Component/AdminComponent.php
@@ -2,7 +2,7 @@
 class AdminComponent extends Component {
 	public $settings = array(
 		'sitename'=>'Your Site',
-		'adminmenu' => array());
+		'menu' => array());
 	
 	public function __construct(ComponentCollection $collection, $settings = array()) {
 		$settings = array_merge($this->settings, (array)$settings);
@@ -20,7 +20,7 @@ class AdminComponent extends Component {
 	public function startup(Controller $controller) {
 		if (isset($controller->params['admin']) && $controller->params['admin']) {
 			$this->Controller = $controller;
-			$controller->set('admin_menu', $this->settings['adminmenu']);
+			$controller->set('admin_menu', $this->settings['menu']);
 			$controller->set('site_name', $this->settings['sitename']);
 			$controller->layout = 'Cms.admin';
 			$this->request = $controller->request;
